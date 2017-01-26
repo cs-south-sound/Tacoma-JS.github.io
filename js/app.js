16107044362565988c6ef9eb4edc4e78999d7076
Joe Devlin <joe@vividventures.biz>
2017-01-25 16:47:04 -0800

    Move react HW component back to index

---
+++ js/app.js
@@ -0,0 +1,14 @@
+/**
+  file: app.js
+
+*/
+
+/**
+    <!-- some react containers-->
+    <!-- copied from the tutorial:
+         https://www.lynda.com/React-js-tutorials/Creating-React-component/519668/542821-4.html?autoplay=true
+    -->
+    This following code does not Babel compile with the inbrowser Babel transpiler
+    because it is not inside the <script> tag the transpiler is looking for
+    and throws an error in the web console
+*/
